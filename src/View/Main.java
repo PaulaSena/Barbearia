9e8ff4db1371e5c0b1dfe7d27dd27b56d06b0331
PaulaSena <senasilvaanapaula@gmail.com>
2019-04-20 13:36:33 -0300

    Model
    Criação do Model

---
+++ src/View/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import model.Servico;
+
+/**
+ *
+ * @author Paula e Weden
+ */
+public class Main {
+   public static void main(String[]args){
+       String nome = "Théo";
+       System.out.println(nome);
+       
+       Servico servico = new Servico(1, "Barba", 30);
+               
+       System.out.println(servico.getDescricao());
+       System.out.println(servico.getValor());
+       
+       
+   }
+}
---
+++ src/model/Servico.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Paula e Weden
+ */
+public class Servico {
+   private int id;
+   private String descricao;
+   private float valor;
+
+    public Servico(int id, String descricao, float valor) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+   
+   
+   
+}
