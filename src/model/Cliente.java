442e62ab0fc51a617f7e5c77e04ccfbaab1c93ef
PaulaSena <senasilvaanapaula@gmail.com>
2019-05-15 09:05:57 -0300

    Model Usuario e Cliente
    Gerando construtor  (Alt+inset) this e get e set

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -5,6 +5,8 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author Paula e Weden
@@ -19,6 +21,82 @@ public class Cliente {
     private String rg;
     private String endereco;
     private String cep;
+
+    public Cliente(int id, String nome, char sexo, Date dataNascimento, String telefone, String email, String rg, String endereco, String cep) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.email = email;
+        this.rg = rg;
+        this.endereco = endereco;
+        this.cep = cep;
+    }
+
+    public Cliente(int id, String nome, char sexo, String telefone, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
     
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public char getSexo(){
+        return sexo;
+    }
+    public void setSexo(char sexo){
+        this.sexo = sexo;
+    }
+    public Date getDataNascimento(){
+        return dataNascimento;
+    }
+    public void setDataNascimento(String Date){
+        this.dataNascimento = dataNascimento;
+    }
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String telefone){
+        this.email = email;
+    }
+    public String getRg(){
+        return rg;
+    }
+    public void setRg(String rg){
+        this.rg = rg;
+    }
+    public String getEndereco(){
+        return endereco;
+    }
+    public void setEndereco(String endereço){
+        this.endereco = endereco;
+    }
     
+    public String getCep(){
+        return cep;
+    }
+    public void setCep(String cep){
+        this.cep = cep;
+        
+    }
+            
 }
